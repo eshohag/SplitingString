b02517a5d7e6c6cde268ccdc6c8cf8da99ae01b9
Md. Shohag Mia <shohag.mia@datasoft-bd.com>
2019-08-03 17:26:53 +0600

    Add project files.

---
+++ SplitingString/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace SplitingString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string value = "1,2,3";
+            var tranMedia = value.Split(new[] { ',', ';' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string s in tranMedia)
+            {
+                Console.WriteLine(Convert.ToInt32(s));
+            }
+
+            string[] configurableFields = Regex.Split("I Love Bangladesh,I hate politics.Test", ",|\\.");
+            foreach (var a in configurableFields)
+            {
+                Console.WriteLine(a);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
